9326526cb0c780c724aadb74e498bbb054c330be
ricardo herbas <ricardosebaherbas@gmail.com>
2026-04-21 19:30:04 +0000

    se realizaron cambios en el index.html, y en el script.js

--- js/script.js
+++ js/script.js
@@ -1,108 +1,105 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-    const form = document.querySelector("form");
-    const input = document.querySelector("#input-tarea");
-    const lista = document.querySelector(".lista-tareas");
+//REFERENCIAS A ELEMENTOS DEL DOM
+const form = document.querySelector("form");
+const input = document.querySelector("#input-tarea");
+const btnAgregar = document.querySelector(".btn-agregar");
+const lista = document.querySelector(".lista-tareas");
 
-    const total = document.querySelector("#total");
-    const pendientes = document.querySelector("#pendientes");
-    const completadas = document.querySelector("#completadas");
+const total = document.querySelector("#total");
+const pendientes = document.querySelector("#pendientes");
+const completadas = document.querySelector("#completadas");
 
-    const btnUltima = document.querySelector(".btn-limpiar-ultima");
-    const btnCompletadas = document.querySelector(".btn-limpiar-completadas");
-    const btnTodas = document.querySelector(".btn-limpiar-todas");
+const btnUltima = document.querySelector(".btn-limpiar-ultima");
+const btnCompletadas = document.querySelector(".btn-limpiar-completadas");
+const btnTodas = document.querySelector(".btn-limpiar-todas");
 
-    // estado de la app
-    let tareas = [];
+// CREACION DE LA LISTA TAREAS
+let tareas = [];
 
-    // AGREGAR TAREA
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
+// FUNCIONES
+//FUNCION PARA AGREGAR TAREA
+function agregarTarea () {
+     const texto = input.value.trim();
+    if (texto === "") return;
 
-        const texto = input.value.trim();
-        if (texto === "") return;
-
-        tareas.push({
-            texto: texto,
-            completada: false
-        });
-
-        input.value = "";
-        renderizar();
+    tareas.push({
+        texto: texto,
+        completada: false
     });
 
-    // RENDERIZAR (Borra la lista actual y vuelve a crear todas las tareas desde el array)
-    function renderizar() {
-        lista.innerHTML = "";
+    input.value = "";
+    actualizarLista();
+}
+//FUNCION PARA ACTUALIZAR LA LISTA (Borra la lista actual y vuelve a crear todas las tareas desde el array)
+function actualizarLista() {
+    lista.innerHTML = "";
 
-        tareas.forEach((tarea, index) => {
+    tareas.forEach((tarea, index) => {
 
-            const li = document.createElement("li");
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = tarea.texto;
 
-            const span = document.createElement("span");
-            span.textContent = tarea.texto;
-
-
-            if (tarea.completada) {
-                span.classList.add("completada");
-            } else {
-                span.classList.remove("completada");
+        if (tarea.completada) {
+            span.style.textDecoration = "line-through";
         }
 
-            // BOTÓN COMPLETAR
-            const btnCompletar = document.createElement("button");
-            btnCompletar.textContent = "✔";
+        const btnCompletar = crearBotonCompletar(index);
 
-            btnCompletar.addEventListener("click", () => {
-                tareas[index].completada = !tareas[index].completada;
-                renderizar();
-            });
+        li.appendChild(span);
+        li.appendChild(btnCompletar);
 
-            // BOTÓN ELIMINAR
-            const btnEliminar = document.createElement("button");
-            btnEliminar.textContent = "✖";
+        lista.appendChild(li);
+    });
 
-            btnEliminar.addEventListener("click", () => {
-                tareas.splice(index, 1);
-                renderizar();
-            });
+    actualizarContadores();
+}
+//FUNCION PAR ACREAR EL BOTÓN "✔" Y SU RESPECTIVO EVENTO
+function crearBotonCompletar (index){
+    const btnCompletar = document.createElement("button");
+    btnCompletar.textContent = "✔";
 
-            li.appendChild(span);
-            li.appendChild(btnCompletar);
-            li.appendChild(btnEliminar);
+    btnCompletar.addEventListener("click", () => {
+        tareas[index].completada = !tareas[index].completada;
+        actualizarLista();
+    });
+    return btnCompletar;
+}
+//FUNCION PARA ACTUALIZAR CONTADORES
+function actualizarContadores() {
+    const totalTareas = tareas.length;
+    const completadasTareas = tareas.filter(t => t.completada).length;
+    const pendientesTareas = totalTareas - completadasTareas;
 
-            lista.appendChild(li);
-        });
+    total.textContent = `Total: ${totalTareas}`;
+    completadas.textContent = `Completadas: ${completadasTareas}`;
+    pendientes.textContent = `Pendientes: ${pendientesTareas}`;
+}
 
-        actualizarContadores();
+// EVENTOS
+//AGREGA TAREA CON CLICK EN EL BOTON "AGREGAR"
+btnAgregar.addEventListener("click", () => {
+    agregarTarea();
+});
+//AGREGA TAREA CON "ENTER"
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault(); 
+        agregarTarea();    
     }
-
-    // CONTADORES
-    function actualizarContadores() {
-        const totalTareas = tareas.length;
-        const completadasTareas = tareas.filter(t => t.completada).length;
-        const pendientesTareas = totalTareas - completadasTareas;
-
-        total.textContent = `Total: ${totalTareas}`;
-        completadas.textContent = `Completadas: ${completadasTareas}`;
-        pendientes.textContent = `Pendientes: ${pendientesTareas}`;
-    }
-
-    // BOTONES FOOTER
-
-    btnUltima.addEventListener("click", () => {
-        tareas.pop();
-        renderizar();
-    });
-
-    btnCompletadas.addEventListener("click", () => {
-        tareas = tareas.filter(t => !t.completada);
-        renderizar();
-    });
-
-    btnTodas.addEventListener("click", () => {
-        tareas = [];
-        renderizar();
-    });
-
+});
+//ELIMINA LA ULTIMA TAREA CON CLICK EN EL BOTON "LIMPIAR ULTIMA"
+btnUltima.addEventListener("click", () => {
+    tareas.pop();
+    actualizarLista();
+});
+//ELIMINA TODAS LAS TAREAS COMPLETADAS CON CLICK EN EL BOTON "LIMPIAR COMPLETADAS"
+btnCompletadas.addEventListener("click", () => {
+    tareas = tareas.filter(t => !t.completada);
+    actualizarLista();
+});
+//ELIMINA TODAS LAS TAREAS CON CLICK EN EL BOTON "LIMPIAR TODAS"
+btnTodas.addEventListener("click", () => {
+    tareas = [];
+    actualizarLista();
 });
